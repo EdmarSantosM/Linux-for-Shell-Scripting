f0feb3380fd6d05aa051ad10600d90a6674bbbca
EdmarSantosM <santoem4@hotmail.com>
2023-12-14 01:28:05 -0300

    Update 02_variables.sh
    Exemplos de variáveis do sistema e do usuário.

--- 02_variables.sh
+++ 02_variables.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-echo "Variáveis definidas pelo usuário"
+echo " Variáveis definidas pelo usuário "
 echo ""
 
-name="Edmar"
+name=" Edmar "
 age=41
 pointer=9.89
 
-echo $name 
-echo $age
-echo "My pointer is: " $pointer
+echo " Nome: " $name 
+echo " Idade: " $age
+echo " Minha pontuação é: " $pointer
+
+echo " First enviroment variable: " $SHELL
+
+echo ""
+
+echo " Nome do usuário:" $USERNAME
+echo " Diretório do usuário:" $HOME
+
+NOW=$( date '+%F_%H:%M:%S')
+echo " Exemplo de uma variável com data pré formatada: " $NOW
