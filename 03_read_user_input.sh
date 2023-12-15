078d3c33cfe459e088165910f9b47dc2bb010d08
EdmarSantosM <santoem4@hotmail.com>
2023-12-15 17:46:53 -0300

    Read user input

---
+++ 03_read_user_input.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# leitura de dado inserido pelo usuário
+
+echo ""
+echo " -------------------------- "
+echo ""
+
+echo " Digite seu nome: "
+read name
+
+echo ""
+echo " Nome informado foi: $name"
+echo ""
+echo " -------------------------- "
