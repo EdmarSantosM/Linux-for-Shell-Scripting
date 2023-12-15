9f08d5ec5673882cda1de2332c6e046c193a758d
EdmarSantosM <santoem4@hotmail.com>
2023-12-15 17:56:11 -0300

    Inserindo múltiplas variáveis no mesmo comando
    read name1 name2 name3 name4

--- 03_read_user_input.sh
+++ 03_read_user_input.sh
@@ -13,3 +13,10 @@ echo ""
 echo " Nome informado foi: $name"
 echo ""
 echo " -------------------------- "
+
+# Inserindo múltiplas váriaveis
+
+echo " Digite os nomes das frutas: "
+read name1 name2 name3 name4
+echo ""
+echo "Nomes das frutas são: $name1, $name2, $name3, $name4"
